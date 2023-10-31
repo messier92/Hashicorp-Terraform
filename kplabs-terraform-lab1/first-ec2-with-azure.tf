b6a9af6eaa116a50ff0e82d0f32d49f09db0116f
Han Long Eugene, Goh <hanlong2102@hotmail.com>
2023-11-01 00:30:32 +0800

    19

---
+++ kplabs-terraform-lab1/first-ec2-with-azure.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "ap-southeast-1"
+    access_key = "MY_ACCCESS_KEY"
+    secret_key = "MY_SECRET_ACCESS_KEY" 
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-036d8cf6cf7a08f64"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "my-first-ec2"
+    }
+}
+
+provider azurerm {}
+
+resource "aws_instance" "web" {
+	ami = ami-123
+	instance_type = "t2.micro"
+}
